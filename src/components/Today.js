cba97e3f95fd851ee0758bb702e40f7696df826b
tticasso <takhanhtung31@gmail.com>
2023-11-29 09:18:33 +0700

    tungtk_FE
    Front-End

---
+++ src/components/Today.js
@@ -0,0 +1,23 @@
+import { Col, Container, Row, NavLink } from "react-bootstrap";
+import "./style/Today.css"
+
+export default function Today() {
+    
+    return (
+        <Container style={{backgroundColor:"#f6f6f8"}}>
+            <Row>
+                <Col style={{backgroundColor:"#ffffff"}} xs={4}>
+                
+                </Col>
+                <Col style={{backgroundColor:"#ffffff"}} xs={4}>
+                
+                </Col>
+                <Col style={{backgroundColor:"#ffffff"}} xs={4}>
+                
+                </Col>
+            </Row>
+       </Container>
+    )
+
+
+}
